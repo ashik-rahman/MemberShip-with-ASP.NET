89447af378e1802cde53a77a7fb615fd518928bb
Ashik <sahkiur52@gmail.com>
2018-01-27 19:20:49 +0600

    admin area and content

--- Memberships/App_Start/BundleConfig.cs
+++ Memberships/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Memberships
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/admin").Include(
+                      "~/Scripts/AdminMenu.js"));
         }
     }
 }
