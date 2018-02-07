264d21209491c7de298712e4b03a73ebeebb07cc
Ashik <sahkiur52@gmail.com>
2018-02-08 01:45:55 +0600

    make a user view model and create a method to get a user first name from the database using Identity

---
+++ Memberships/Extensions/IdentityExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Memberships.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Memberships.Extensions
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserFirstName(this IIdentity identity)
+        {
+            var db = ApplicationDbContext.Create();
+            var user = db.Users.FirstOrDefault(u => u.UserName.Equals(identity.Name));
+
+            return user != null ? user.FirstName : String.Empty;
+        }
+    }
+}
---
+++ Memberships/Models/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserViewModel
+    {
+        [Display(Name ="User Id")]
+        public int Id { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+        [Display(Name ="First Name")]
+        [StringLength(30, ErrorMessage ="The {0} must be at least {1} characters long",MinimumLength =2)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
