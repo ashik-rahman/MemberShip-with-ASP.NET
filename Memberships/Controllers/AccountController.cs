f2c069c78c07d312201a88486a39f539880d253a
Ashik <sahkiur52@gmail.com>
2018-02-13 01:43:03 +0600

    details of user & subscription, added a specific subscription to a user

--- Memberships/Controllers/AccountController.cs
+++ Memberships/Controllers/AccountController.cs
@@ -12,6 +12,8 @@ using Memberships.Models;
 using System.Collections.Generic;
 using Memberships.Extensions;
 using System.Net;
+using System.Data.Entity;
+using Memberships.Entities;
 
 namespace Memberships.Controllers
 {
@@ -636,6 +638,88 @@ namespace Memberships.Controllers
             }
             catch { }
             return View(model);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Subscriptions(string userId)
+        {
+            if (userId == null || userId.Equals(string.Empty))
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var model = new UserSubscriptionViewModel();
+            var db = new ApplicationDbContext();
+            model.UserSubscriptions = await (from us in db.UserSubscriptions
+                                             join s in db.Subscriptions on us.SubscriptionId equals s.Id
+                                             where us.UserId.Equals(userId)
+                                             select new UserSubscriptionModel
+                                             {
+                                                 Id = us.SubscriptionId,
+                                                 StartDate = us.StartDate,
+                                                 EndDate = us.EndDate,
+                                                 Description = s.Description,
+                                                 RegistrationCode = s.RegistrationCode,
+                                                 Title = s.Title
+                                             }).ToListAsync();
+            var ids = model.UserSubscriptions.Select(us => us.Id);
+            model.Subscriptions = await db.Subscriptions.Where(
+                        s => !ids.Contains(s.Id)).ToListAsync();
+
+            model.DisableDropDown = model.Subscriptions.Count.Equals(0);
+            model.UserId = userId;
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Subscriptions(UserSubscriptionViewModel model)
+        {
+            try
+            {
+                if (model == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                if (ModelState.IsValid)
+                {
+                    var db = new ApplicationDbContext();
+                    db.UserSubscriptions.Add(new UserSubscription
+                    {
+                        UserId = model.UserId,
+                        SubscriptionId = model.SubscriptionId,
+                        StartDate = DateTime.Now,
+                        EndDate = DateTime.MaxValue,
+                    });
+
+                    await db.SaveChangesAsync();
+                }
+            }
+            catch { }
+            return RedirectToAction("Subscriptions", "Account", new { UserId = model.UserId});
+        }
+        
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> RemoveUserSubscription(string userId, int subscriptionId)
+        {
+            try
+            {
+                if (userId ==null || userId.Length.Equals(0) || subscriptionId <=0)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                if (ModelState.IsValid)
+                {
+                    var db = new ApplicationDbContext();
+                    var subscriptions = db.UserSubscriptions.Where(
+                        us=>us.UserId.Equals(userId) && us.SubscriptionId.Equals(subscriptionId)
+                        );
+                    db.UserSubscriptions.RemoveRange(subscriptions);
+
+                    await db.SaveChangesAsync();
+                }
+            }
+            catch { }
+            return RedirectToAction("Subscriptions", "Account", new { UserId = userId });
         }
     }
 }
---
+++ Memberships/Models/UserSubscriptionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Memberships.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Models
+{
+    public class UserSubscriptionViewModel
+    {
+        public ICollection<Subscription> Subscriptions { get; set; }
+        public ICollection<UserSubscriptionModel> UserSubscriptions { get; set; }
+        public bool DisableDropDown { get; set; }
+        public string UserId { get; set; }
+        public int SubscriptionId { get; set; }
+
+    }
+}
